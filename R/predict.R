65bbf22ce664ef9f1d6ddc12dd7300726021a13a
Jiaying Liao <jiaying.liao@qq.com>
2023-03-09 09:20:38 -0800

    first commit

---
+++ R/predict.R
@@ -0,0 +1,19 @@
+classify_penguin <- function(bill_length, flipper_length) {
+  
+  stopifnot(is_numeric(bill_length) && is_numeric(flipper_length))
+  stopifnot(bill_length>0 && flipper_length>0)
+  
+  if (flipper_length>205) {
+    return("Gentoo")
+  } else {
+    if (bill_length>45) {
+      return("Chinstrap")
+    } else {
+      return("Adelle")
+    }
+  }
+}
+testthat::expect_equal(classify_penguin(40,210), "Gentoo")
+testthat::expect_equal(classify_penguin(40,200), "Adelle")
+testthat::expect_error(classify_penguin("zzz",10))
+testthat::expect_error(classify_penguin(0,-5))
